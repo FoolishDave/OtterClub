4ac4053fc38684b2a42d4780193d16ad39602056
Albert Ma <albertfma@gmail.com>
2017-01-21 18:17:46 -0500

    Spawn increase

---
+++ Assets/SpawnUpgrade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnUpgrade : MonoBehaviour {
+    /// <summary>
+    /// Button observed
+    /// </summary>
+    public Button yourButton;
+    public GameObject Master;
+    public SpawnScript spawner;
+    // Use this for initialization
+    void Start () {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    
+    void TaskOnClick()
+    {
+
+        //Debug.Log(sp._maxSpawnedEnemies);
+        Debug.Log(spawner.maxSpawnedEnemies);
+        spawner.maxSpawnedEnemies = spawner.maxSpawnedEnemies+1;
+        Debug.Log(spawner.maxSpawnedEnemies);
+    }
+}
