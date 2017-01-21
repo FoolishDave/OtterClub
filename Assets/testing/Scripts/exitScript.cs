277533876d43c5b34119322d6a59a1289e5c53b6
Chris Leight <leight.7@buckeyemail.osu.edu>
2017-01-21 13:38:30 -0500

    Adding initial enemy and ui

---
+++ Assets/testing/Scripts/exitScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script to exit game on button press
+/// </summary>
+public class exitScript : MonoBehaviour {
+
+    /// <summary>
+    /// Button observed
+    /// </summary>
+    public Button yourButton;
+
+    /// <summary>
+    /// Gets the button and adds a listener to it
+    /// </summary>
+    void Start () {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    /// <summary>
+    /// Exits application on click
+    /// </summary>
+    void TaskOnClick()
+    {
+        Debug.Log("Exit application");
+        Application.Quit();
+    }
+}
